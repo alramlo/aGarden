b2d233d16fc9435ebc81c112f8142fcf1bbe8303
Alberto <alberto.ramirez.losilla@gmail.com>
2014-08-05 00:12:37 +0200

    Check internet at Login

--- aGarden/src/main/java/es/uvp/agarden/SesionActivity.java
+++ aGarden/src/main/java/es/uvp/agarden/SesionActivity.java
@@ -1,8 +1,13 @@
 package es.uvp.agarden;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by Alberto on 18/07/14.
@@ -16,6 +21,46 @@ public class SesionActivity extends ActionBarActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    public void login(View view){
+
+        //Comprobamos si hay conexión de datos
+        if (checkInternetConnection(getApplicationContext())==false){
+            Toast toast = Toast.makeText(getApplicationContext(),R.string.noInternet,Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else{
+
+            //Llamar al servicio rest
+
+        }
+
+
+    }
+
+    //Método para comprobar si hay conexión de datos
+    public boolean checkInternetConnection(Context context){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager!=null){
+
+            NetworkInfo info3G = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            NetworkInfo infoWifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+            //Comprobamos si hay conexión 3G
+            if( info3G!=null && info3G.isAvailable() && info3G.getDetailedState() == NetworkInfo.DetailedState.CONNECTED )
+                return true;
+
+            //Comprobamos si hay conexión WIFI
+            if( infoWifi!=null && infoWifi.isAvailable() && infoWifi.getDetailedState() == NetworkInfo.DetailedState.CONNECTED )
+                return true;
+
+        }
+
+        //No hay ninguna conexión disponible
+        return false;
     }
 
 }
